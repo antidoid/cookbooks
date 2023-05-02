71a9e7a5bce556cd5e2cd2640634b5863ffc84b3
Piyush Singh <singhpiyush998@gmail.com>
2023-05-02 20:49:19 +0530

    Add: router of different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { Home, Login } from "./pages";
 
 function App() {
-  return <h1>Hello from frontend</h1>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center text-white mx-5 mt-2 bg-zinc-900 bg-opacity-40 rounded-3xl px-20 py-2">
+      <span className="text-2xl font-extrabold">Cookbooks</span>
+      <ul className="flex grow justify-evenly mx-20">
+        <li>HOME</li>
+        <li>CATEGORIES</li>
+        <li>EXPLORE</li>
+        <li>ABOUT US</li>
+      </ul>
+      <Link to="/login">
+        <p className="py-2 px-2 text-md bg-teal-500 rounded-lg font-bold">
+          Login
+        </p>
+      </Link>
+    </nav>
+  );
+};
