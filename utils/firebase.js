2747b53f3929e0961a056f90fe9184204b03581d
Piyush Singh <singhpiyush998@gmail.com>
2023-05-02 14:47:24 +0530

    FIX: firebase config

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -10,4 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export default app;
+const auth = getAuth();
+export default auth;
