46372b3da7f1def15803caef61a884b9c9c378c0
Piyush Singh <singhpiyush998@gmail.com>
2023-05-24 11:20:11 +0530

    Add: Support for knowing if the current user is logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
+import auth from "../../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const [user] = useAuthState(auth);
+  const [signOut, loading, error] = useSignOut(auth);
+  const navigate = useNavigate();
+
+  const logoutCurrentUser = async () => {
+    const isLoggedOut = await signOut(auth);
+    if (isLoggedOut) navigate("/");
+  };
+
   return (
     <nav className="flex justify-between items-center text-white mx-5 mt-2 bg-zinc-900 bg-opacity-40 rounded-3xl px-20 py-2">
       <span className="text-2xl font-extrabold">Cookbooks</span>
       <ul className="flex grow justify-evenly mx-20">
-        <li>HOME</li>
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
         <li>CATEGORIES</li>
         <li>
           <Link to="/explore">EXPLORE</Link>
         </li>
         <li>ABOUT US</li>
       </ul>
-      <Link to="/login">
-        <p className="py-2 px-2 text-md bg-teal-500 rounded-lg font-bold">
-          Login
-        </p>
-      </Link>
+      {user ? (
+        <button onClick={logoutCurrentUser}>
+          <p className="py-2 px-2 text-md bg-teal-500 rounded-lg font-bold">
+            Logout
+          </p>
+        </button>
+      ) : (
+        <Link to="/login">
+          <p className="py-2 px-2 text-md bg-teal-500 rounded-lg font-bold">
+            Login
+          </p>
+        </Link>
+      )}
     </nav>
   );
 };
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../utils/firebase.js";
 
 const Explore = () => {
+    const [user, loading] = useAuthState(auth);
     return (
-        <div>
-            <span>Welcome to Cookboks</span>
-        </div>
+        <>
+            {loading && <p>Please wait, loading user</p>}
+            {user && (
+                <div>
+                    <span>Welcome to Cookboks, {user.displayName}</span>
+                </div>
+            )}
+        </>
     );
 };
 
