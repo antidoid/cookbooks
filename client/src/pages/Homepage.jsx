e4c3adc5ec65abe301ec52ea83a8fc6ff515e8eb
antidoid <singhpiyush998@gmail.com>
2024-06-21 14:21:53 +0530

    initial commit

--- client/src/pages/Homepage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Homepage() {
-  return (
-    <div>
-      <h1>Welcome to Cookbooks</h1>
-    </div>
-  );
-}
--- client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Login } from "./Login";
-export { default as Homepage } from "./Homepage";
-export { default as About } from "./About";
-export { default as Recipe } from "./Recipe";
-export { default as NotFound } from "./NotFound";
